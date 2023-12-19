3e9da4e7f83b933c063ef598faaa30aa82c0deae
48845 a <miror49735@sportrid.com>
2023-12-19 12:38:45 +0000

    start project

---
+++ README.md
@@ -0,0 +1,35 @@
+# blog with wagtail
+Version: 0.0.1
+with python3 and django
+
+## #Install
+
+> 👉 Download the code  
+
+```bash
+$ git clone https://github.com/irAbs174/blog.git
+$ cd blog
+```
+
+<br />
+
+> 👉 Build Project
+
+```bash
+$ docker build -t blog .
+```
+
+<br />
+
+> 👉 RUN Project
+
+```bash
+$ docker run -p 8000:8000 blog
+```
+
+<br />
+
+## Communication
+mail: damerchiloa@gmail.com
+github: @irAbs174
+
---
+++ core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t=g*^p#uj8b(0yox(s)l3^hb8ndy6@lduvlnf$#z9(dd_3k-p_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
